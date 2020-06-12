56096ae8b750a27a817ba2d59d5f99d55b28c06b
kimyoungjae96 <sskim5421@gmail.com>
2020-06-13 01:26:17 +0900

    20200613

--- screen/result/dailyRes.js
+++ screen/result/dailyRes.js
@@ -1,87 +1,87 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet,Image, Button} from "react-native";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 import { Container, Content } from "native-base";
 
 function dailyRes() {
-    return(
-        <>
-        <View style = {styles.dailyView}>
-        <View style = {styles.dailyresultBox}>
-            <View style = {styles.boxImages}>
-                <Image
-                    source={require("./img/70percent.png")}
-                    style={styles.teethImage}>
-                </Image>
-            </View>
-            <View style = {styles.boxContents}>
-                <Text style = {styles.boxContentsTitle}>6월 14일 12:00</Text>
-                <Text style = {styles.boxContentsContent}>냠냠점수 80</Text>
-            </View>
+  return (
+    <>
+      <View style={styles.dailyView}>
+        <View style={styles.dailyresultBox}>
+          <View style={styles.boxImages}>
+            <Image
+              source={require("./img/70percent.png")}
+              style={styles.teethImage}
+            ></Image>
+          </View>
+          <View style={styles.boxContents}>
+            <Text style={styles.boxContentsTitle}>6월 14일 12:00</Text>
+            <Text style={styles.boxContentsContent}>냠냠점수 80</Text>
+          </View>
         </View>
-        <View style = {styles.dailyresultBox}>
-            <View style = {styles.boxImages}>
-                <Image
-                    source={require("./img/50percent.png")}
-                    style={styles.teethImage}>
-                </Image>
-            </View>
-            <View style = {styles.boxContents}>
-                <Text style = {styles.boxContentsTitle}>6월 13일 08:00</Text>
-                <Text style = {styles.boxContentsContent}>냠냠점수 50</Text>
-            </View>
+        <View style={styles.dailyresultBox}>
+          <View style={styles.boxImages}>
+            <Image
+              source={require("./img/50percent.png")}
+              style={styles.teethImage}
+            ></Image>
+          </View>
+          <View style={styles.boxContents}>
+            <Text style={styles.boxContentsTitle}>6월 13일 08:00</Text>
+            <Text style={styles.boxContentsContent}>냠냠점수 50</Text>
+          </View>
         </View>
-        <View style = {styles.dailyresultBox}>
-            <View style = {styles.boxImages}>
-                <Image
-                    source={require("./img/100percent.png")}
-                    style={styles.teethImage}>
-                </Image>
-            </View>
-            <View style = {styles.boxContents}>
-                <Text style = {styles.boxContentsTitle}>6월 12일 15:00</Text>
-                <Text style = {styles.boxContentsContent}>냠냠점수 100</Text>
-            </View>
+        <View style={styles.dailyresultBox}>
+          <View style={styles.boxImages}>
+            <Image
+              source={require("./img/100percent.png")}
+              style={styles.teethImage}
+            ></Image>
+          </View>
+          <View style={styles.boxContents}>
+            <Text style={styles.boxContentsTitle}>6월 12일 15:00</Text>
+            <Text style={styles.boxContentsContent}>냠냠점수 100</Text>
+          </View>
         </View>
-        </View>
-        </>
-    );
+      </View>
+    </>
+  );
 }
 const styles = StyleSheet.create({
-    dailyView:{
-        flex:1,
-    },
-  dailyresultBox:{
-    backgroundColor:"white",
-    height:100,
-    width:300,
-    borderWidth:1,
-    borderColor:'gray',
-    borderRadius:10,
-    margin:20,
-    padding:10,
-    flexDirection:'row'
+  dailyView: {
+    flex: 1,
   },
-  boxContents:{
-      flex:2,
-      backgroundColor:"white",
+  dailyresultBox: {
+    backgroundColor: "white",
+    height: 100,
+    width: 300,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 10,
+    margin: 20,
+    padding: 10,
+    flexDirection: "row",
   },
-  boxContentsTitle:{
-      fontSize:15,
-      fontWeight:"bold",
+  boxContents: {
+    flex: 2,
+    backgroundColor: "white",
   },
-  boxContentsContent:{
-      marginTop:10,
-      fontSize:15,
+  boxContentsTitle: {
+    fontSize: 15,
+    fontWeight: "bold",
   },
-  boxImages:{
-      flex:1,
-      alignItems:"center",
-      justifyContent:"center",
-      marginRight:10
+  boxContentsContent: {
+    marginTop: 10,
+    fontSize: 15,
   },
-  teethImage:{
-      width:60,
-      height:60,
-  }
+  boxImages: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+  },
+  teethImage: {
+    width: 60,
+    height: 60,
+  },
 });
 export default dailyRes;
