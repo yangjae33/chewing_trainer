d2402c94e428f4bfe26086bbf42a98c9eeb2da9a
kimyoungjae96 <sskim5421@gmail.com>
2020-06-17 16:52:07 +0900

    data 통신 완료

--- screen/setting/setting.js
+++ screen/setting/setting.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Header from "../Common/Header";
+import Axios from "axios";
 
 function Setting({ navigation }) {
+  const [count, setCount] = useState(0);
+  const temp = () => {
+    Axios({
+      method: "GET",
+      url: "http://crerarlab.com/homepage/temp_get.php",
+      headers: { "Content-Type": "multipart/form-data" },
+    }).then((response) => {
+      console.log("엥", response);
+      setCount(response.data.result);
+    });
+  };
+  setInterval(temp, 1000);
+
   return (
     <>
       <Header titleText="설정" prev={true} navigation={navigation}></Header>
       <View style={styles.settingView}>
+        <Text>앱 통신 값:{count}</Text>
         <Text>준비중입니다.</Text>
       </View>
     </>
